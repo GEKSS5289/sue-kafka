a5d8a4bc9dfebba04ae4305c123a005e0ce34ab1
GEKSS5289 <496966859@qq.com>
2020-08-17 16:19:30 +0800

    kafka序列化

--- src/main/java/com/sue/kafka/api/serializer/DeserializerConsumer.java
+++ src/main/java/com/sue/kafka/api/serializer/DeserializerConsumer.java
@@ -1,5 +1,6 @@
 package com.sue.kafka.api.serializer;
 
+import com.sue.kafka.api.entity.User;
 import com.sue.kafka.api.topic.Const;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -23,7 +24,7 @@ public class DeserializerConsumer {
         Properties properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.182.150:9092");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, UserDeSerializer.class.getName());
 
         //非常重要的属性配置 与我们消费者订阅组有关系
         properties.put(ConsumerConfig.GROUP_ID_CONFIG,"serial-group");
@@ -34,7 +35,7 @@ public class DeserializerConsumer {
         properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,5000);
 
         //创建消费者对象
-        KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
+        KafkaConsumer<String, User> consumer = new KafkaConsumer<>(properties);
         //订阅你感兴趣的主题
         consumer.subscribe(Collections.singletonList(Const.TOPIC_SERIAL));
         System.err.println("quickstart consumer started...");
@@ -44,12 +45,12 @@ public class DeserializerConsumer {
                 //等待多久拉取一次消息
                 //拉取TOPIC_QUICKSTART主题里面的所有消息
                 //topic 和 partition是一对多关系 一个topic可以用多个partition
-                ConsumerRecords<String, String> poll = consumer.poll(Duration.ofMillis(1000));
+                ConsumerRecords<String, User> poll = consumer.poll(Duration.ofMillis(1000));
                 //因为partition中存储的，所以需要遍历partition集合
                 poll.partitions().forEach(topicPartition -> {
 
                     //通过TopicPartition获取到实际records对象中数据集合
-                    List<ConsumerRecord<String, String>> records = poll.records(topicPartition);
+                    List<ConsumerRecord<String, User>> records = poll.records(topicPartition);
                     //获取当前topicPartition消息条数
                     int size = records.size();
                     //获取到TopicPartition对应的主题名称
@@ -63,11 +64,11 @@ public class DeserializerConsumer {
 
                     for(int i = 0;i<size;i++){
                         //实际数据
-                        ConsumerRecord<String, String> consumerRecord = records.get(i);
-                        String value = consumerRecord.value();
+                        ConsumerRecord<String, User> consumerRecord = records.get(i);
+                        User value = consumerRecord.value();
                         long offset = consumerRecord.offset();
                         long commitOffset = offset+1;
-                        System.out.println(String.format("获取实际消息value:%s,消息offset:%s,提交offset:%s",value,offset,commitOffset));
+                        System.out.println(String.format("获取实际消息value:%s,消息offset:%s,提交offset:%s",value.getName(),offset,commitOffset));
                     }
                 });
             }
--- src/main/java/com/sue/kafka/api/serializer/UserDeSerializer.java
+++ src/main/java/com/sue/kafka/api/serializer/UserDeSerializer.java
@@ -1,9 +1,55 @@
 package com.sue.kafka.api.serializer;
 
+import com.sue.kafka.api.entity.User;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
 /**
  * @author sue
  * @date 2020/8/17 15:25
  */
 
-public class UserDeSerializer {
+public class UserDeSerializer implements Deserializer<User> {
+    @Override
+    public void configure(Map<String, ?> map, boolean b) {
+
+    }
+
+    @Override
+    public User deserialize(String s, byte[] bytes) {
+        if(bytes == null){
+            return null;
+        }
+        if(bytes.length < 8){
+            throw new SerializationException("size is wrong ,must be data length>=8");
+        }
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        int idLen = byteBuffer.getInt();
+        byte[] idByte = new byte[idLen];
+        byteBuffer.get(idByte);
+
+        int nameLen = byteBuffer.getInt();
+        byte[] nameByte = new byte[nameLen];
+        byteBuffer.get(nameByte);
+
+        String id;
+        String name;
+        try {
+            id = new String(idByte,"UTF-8");
+            name = new String(nameByte,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new SerializationException("deserializing error!",e);
+        }
+
+        return new User(id,name);
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
